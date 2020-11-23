a0ea4da66c70f6841176b2dee8c30f30e05cbfc7
wolsz <wolsz1@gmail.com>
2020-11-22 20:01:55 -0500

    After adding fragments and converting the fragments to the appropriate layouts.

--- starter/app/src/main/java/com/udacity/shoestore/view/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udacity.shoestore.R
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
